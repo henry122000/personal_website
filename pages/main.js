3dd0a614ae2c6e8027d64b1cd437500a8bcd09a7
Henry Dao <mhoang0102@gmail.com>
2023-09-08 11:56:04 -0700

    javascript for about

--- pages/main.js
+++ pages/main.js
@@ -5,3 +5,15 @@ var typed= new Typed(".text", {
     backDelay: 1000,
     loop: true
 })
+var section_links = document.getElementsByClassName("sectionLink");
+var section_contents = document.getElementsByClassName("sectionContent");
+function openSection(sectionName){
+    for(section_link of section_links){
+        section_link.classList.remove("activeLink");
+    }
+    for(section_content of section_contents){
+        section_content.classList.remove("activeContent");
+    }
+    event.currentTarget.classList.add("activeLink");
+    document.getElementById(sectionName).classList.add("activeContent");
+}
