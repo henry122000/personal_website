e9f5fbb246edac8c16c64bbaca54906d92051020
Henry Dao <mhoang0102@gmail.com>
2023-09-17 13:05:28 -0700

    scroll content animation

--- pages/main.js
+++ pages/main.js
@@ -34,6 +34,11 @@ window.onscroll = () => {
                 link.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            sec.classList.add('scrollAnimate');
+        }
+        else{
+            sec.classList.remove('scrollAnimate');
+
         }
     });
     
